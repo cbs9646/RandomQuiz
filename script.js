e1b4efe7d8c8546aaaa2f633096d0aa257783f47
Charlie Stedman <9369467+cbs9646@users.noreply.github.com>
2021-11-16 08:39:58 -0600

    updated index with buttons

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let timerEl = document.querySelector(".timer");
 let secondsLeft = 60;
-const btn = document.querySelector(".button");
-
-
-
-btn.addEventListener('click', setTime);
+const btn = document.getElementsByClassName("button");
+btn.addEventListener('click', timerEl);
 
 
 
 function setTime(){
 
 let countdownTimer = setInterval(function() {
-    secondsLeft--;
+ 
+   secondsLeft--;
     timerEl.textContent = "Timer: " + secondsLeft;
-    
+
+ 
+
     if(secondsLeft ===0) {
         clearInterval(countdownTimer);
     }
@@ -25,26 +25,41 @@ let countdownTimer = setInterval(function() {
 
 setTime();
 
-let questions = [
-{   question: "What element is denoted by the chemical symbol Sn in the periodic table?",
-    answers: ["Tin", "Gold", "Silver", "Platinum"]
-    correctAnswer: 0; 
-}
-{   question: "Which rock band was founded by Trent Reznor in 1988?",
-    answers: ["Korn", "Pink Floyd", "Radiohead", "Nine Inch Nails"]
-    correctAnswer: 3; 
-}
-{   question: "What is the smallest planet in our solar system?",
-    answers: ["Pluto", "Earth", "Mercury", "Saturn"]
-    correctAnswer: 2; 
-}
-{   question: "What was the Turkish city of Istanbul called before 1930?",
-    answers: ["Dallas", "Constantinople", "Turkey", "St. Petersburg"]
-    correctAnswer: 1; 
-}
+// let questions = [
+// {   question: "What element is denoted by the chemical symbol Sn in the periodic table?",
+//     answers: ["Tin", "Gold", "Silver", "Platinum"],
+//     correctAnswer: 0
+// },
+// {   question: "Which rock band was founded by Trent Reznor in 1988?",
+//     answers: ["Korn", "Pink Floyd", "Radiohead", "Nine Inch Nails"],
+//     correctAnswer: 3 
+// },
+// {   question: "What is the smallest planet in our solar system?",
+//     answers: ["Pluto", "Earth", "Mercury", "Saturn"],
+//     correctAnswer: 2
+// },
+// {   question: "What was the Turkish city of Istanbul called before 1930?",
+//     answers: ["Dallas", "Constantinople", "Turkey", "St. Petersburg"],
+//     correctAnswer: 1
+// },
+
+// console.log(questions)
+
+
+// ];
+
+// let startingQuestion = 0;
 
 
 
 
 
-]
+// for (let i = 0; i < questions.length; i++) {
+//     const element = questions[i];
+    
+//     questions[i].answers
+
+//     for (let i = 0; i < questions[i].answers.length; i++){
+//         const element = questions[i].answers[i];    
+//     }
+// }
